ab88e29b1ca5a3081ebbeefe7fe98e7af6e0ba3c
Felipe Belinassi <felipebelinassi@gmail.com>
2021-04-23 23:07:57 +0200

    Clean initial files and setup initial css variables

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,20 +1,7 @@
-import * as S from './styles';
-
 const Main = () => (
-  <S.Wrapper>
-    <S.Logo
-      src="/img/logo.svg"
-      alt="Imagem de um átomo e React Boilerplate escrito ao lado"
-    />
-    <S.Title>React Boilerplate</S.Title>
-    <S.Description>
-      TypeScript, ReactJS, NextJS e Styled Components
-    </S.Description>
-    <S.Illustration
-      src="/img/hero-illustration.svg"
-      alt="Um desenvolvedor de frente para uma tela com código"
-    />
-  </S.Wrapper>
+  <div>
+    <h1>NextJS na Prática</h1>
+    <a href="#">Link</a>
+  </div>
 );
-
 export default Main;
